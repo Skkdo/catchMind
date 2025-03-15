39f736353940135b696586e75c97b7027394be32
박진형 <skill1218@naver.com>
2025-03-15 15:06:42 +0900

    init

---
+++ src/test/java/project/catchMind/CatchMindApplicationTests.java
@@ -0,0 +1,13 @@
+package project.catchMind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchMindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
